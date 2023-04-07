e06dc57a97cf640994f9b511fdf6b542fa95768f
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-04-07 21:22:52 +0200

    update app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "hsl(0, 100%, 50%)"];
+
+const btn = document.getElementById('btn'); // this is where you needed the btn id on the button tag
+const color =  document.querySelector(".color");
+
+//This is for the event listener
+                            //here just add an anonymous function
+    //the first task is to target background to change color once the button is clicked
+btn.addEventListener('click', function(){
+   //1. console.log(document.body); this is to check if the button is clicked the body element will be logged into the console.
+})
