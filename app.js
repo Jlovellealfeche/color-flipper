e10ce8ca43584eb62e95718172cb93793efde597
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-04-07 21:33:34 +0200

    .

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "hsl(0, 100%, 50%)"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "hsl(60, 60%, 80%)"];
 
 const btn = document.getElementById('btn'); // this is where you needed the btn id on the button tag
 const color =  document.querySelector(".color");
@@ -9,4 +9,11 @@ const color =  document.querySelector(".color");
     //the first task is to target background to change color once the button is clicked
 btn.addEventListener('click', function(){
    //1. console.log(document.body); this is to check if the button is clicked the body element will be logged into the console.
+   // 2. the goal is to get random number between 0-5 color[4] 
+   const randomNumber = 4;
+   document.body.style.backgroundColor = colors[randomNumber];
+   color.textContent = colors[randomNumber]
 })
+
+
+//13:37 to be continued
